16d98a48bd3b6ac908d5805e6dc4268660700cff
wangrongrong <rr.wang@zuolin.com>
2018-01-03 10:51:41 +0800

    添加

---
+++ GestureUnClock/GestureUnClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestureUnClock
+//
+//  Created by Monster on 2018/1/3.
+//  Copyright © 2018年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
